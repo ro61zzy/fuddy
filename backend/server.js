49553edcf26aa1b22464fd4258fc8a29f4a727bb
Rose Karemeri <rosekaremeri@gmail.com>
2022-08-24 23:07:17 +0300

    Server n db

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express"); //all the things we will need
+const cors = require("cors");
+const mongoose = require("mongoose"); //help connect mongodb
+require("dotenv").config(); //configures so we have our .env file
+
+const app = express(); // creates express server and show port it will run on
+const port = process.env.PORT || 5000;
+
+app.use(cors()); //cors middleware
+app.use(express.json()); //sending and receiving json
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+app.listen(port, () => {
+  //starts the server
+  console.log(`Server is running on port: ${port}`);
+});
